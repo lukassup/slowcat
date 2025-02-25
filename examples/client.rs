2672af91b0917df05cc6f16bdeefac830b21f504
Lukas Supienis <lukas.supienis@vinted.com>
2025-02-26 01:15:31 +0200

    server loop accept, tidy stuff

--- examples/client.rs
+++ examples/client.rs
@@ -1,15 +1,15 @@
 use slowcat::*;
 
+use std::io;
+use std::mem;
 use std::net::Ipv4Addr;
-use std::{io, mem};
 
 use clap::Parser;
 
-const RX_BUFFER_SIZE: usize = 256;
+const RX_BUFFER_SIZE: usize = 8192;
 type RxBuffer = [u8; RX_BUFFER_SIZE];
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long)]
     ip: Ipv4Addr,
@@ -22,52 +22,39 @@ fn main() -> Result<(), io::Error> {
     let remote_addr = args.ip.to_bits();
     let remote_port = args.port;
 
-    // 1. socket()
+    // 1
     let sockfd = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0)?;
-    #[cfg(any(
-        target_os = "dragonfly",
-        target_os = "freebsd",
-        target_os = "netbsd",
-        target_vendor = "apple"
-    ))]
+    #[cfg(target_vendor = "apple")]
     // By default write() to a closed socket sends a signal. Setting
     // SO_NOSIGPIPE throws EPIPE error instead which is easy to handle
     setsockopt(sockfd, libc::SOL_SOCKET, libc::SO_NOSIGPIPE, 1)?;
 
-    // 2. connect()
+    // 2
     connect(
         sockfd,
         sockaddr_in {
-            #[cfg(any(
-                target_os = "dragonfly",
-                target_os = "freebsd",
-                target_os = "netbsd",
-                target_vendor = "apple"
-            ))]
-            sin_len: 0,
-
-            sin_family: AF_INET as libc::sa_family_t, // IPv4
+            sin_family: AF_INET /* IPv4 */ as libc::sa_family_t,
             // NOTE: s_addr & sin_port are in network byte order (big endian)
             sin_addr: in_addr {
                 s_addr: remote_addr.to_be() as libc::in_addr_t,
             },
             sin_port: remote_port.to_be() as libc::in_port_t,
-            sin_zero: [0; 8],
+            ..unsafe { mem::zeroed() }
         },
     )?;
 
-    // 3. write()
-    let message = "PING";
+    // 3
+    let message = "PING\n\n";
     let tx_bytes = write(sockfd, message.as_bytes())?;
-    println!("[tx_len={tx_bytes}] -> {message}");
+    println!("[tx_len={tx_bytes}] -> {message:?}");
 
-    // 4. read()
-    let mut buffer: RxBuffer = unsafe { mem::zeroed() };
-    let rx_bytes = read(sockfd, &mut buffer)?;
-    let s = std::str::from_utf8(&buffer).unwrap_or("");
-    println!("[rx_len={rx_bytes}] <- {s}");
+    // 4
+    let mut buffer: RxBuffer = [0; RX_BUFFER_SIZE];
+    let rx_bytes = read(sockfd, &mut buffer)? as usize;
+    let rx_msg = std::str::from_utf8(&buffer[0..rx_bytes]).unwrap_or("");
+    println!("[rx_len={rx_bytes}] <- {rx_msg:?}");
 
-    // 5. close()
+    // 5
     close(sockfd)?;
 
     Ok(())
--- examples/server.rs
+++ examples/server.rs
@@ -1,16 +1,16 @@
 use slowcat::*;
 
+use std::io;
+use std::mem;
 use std::net::Ipv4Addr;
-use std::{io, mem};
 
 use clap::Parser;
 
 const LISTEN_BACKLOG: i32 = 5;
-const RX_BUFFER_SIZE: usize = 256;
+const RX_BUFFER_SIZE: usize = 8192;
 type RxBuffer = [u8; RX_BUFFER_SIZE];
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long)]
     ip: Ipv4Addr,
@@ -23,64 +23,50 @@ fn main() -> Result<(), io::Error> {
     let listen_addr = args.ip.to_bits();
     let listen_port = args.port;
 
-    // 1. socket()
+    // 1
     let listen_sockfd = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0)?;
-    #[cfg(any(
-        target_os = "dragonfly",
-        target_os = "freebsd",
-        target_os = "netbsd",
-        target_vendor = "apple"
-    ))]
+    #[cfg(target_vendor = "apple")]
     // By default write() to a closed socket sends a signal. Setting
     // SO_NOSIGPIPE throws EPIPE error instead which is easy to handle
     setsockopt(listen_sockfd, libc::SOL_SOCKET, libc::SO_NOSIGPIPE, 1)?;
-
-    // 2. bind()
     #[cfg(not(windows))]
-    // SO_REUSEADDR allows reusing recently closed sockets in TIME_WAIT state
     setsockopt(listen_sockfd, libc::SOL_SOCKET, libc::SO_REUSEADDR, 1)?;
+
+    // 2
     bind(
         listen_sockfd,
         sockaddr_in {
-            #[cfg(any(
-                target_os = "dragonfly",
-                target_os = "freebsd",
-                target_os = "netbsd",
-                target_vendor = "apple"
-            ))]
-            sin_len: 0,
-
-            sin_family: AF_INET as libc::sa_family_t, // IPv4
+            sin_family: AF_INET /* IPv4 */ as libc::sa_family_t,
             // NOTE: s_addr & sin_port are in network byte order (big endian)
             sin_addr: in_addr {
                 s_addr: listen_addr.to_be() as libc::in_addr_t,
             },
             sin_port: listen_port.to_be() as libc::in_port_t,
-            sin_zero: [0; 8],
+            ..unsafe { mem::zeroed() }
         },
     )?;
 
-    // 3. listen()
+    // 3
     listen(listen_sockfd, LISTEN_BACKLOG)?;
 
-    // 4. accept()
-    let client_sockfd = accept(listen_sockfd)?;
+    // 4
+    while let Ok(client_sockfd) = accept(listen_sockfd) {
+        // 5
+        let mut buffer: RxBuffer = [0; RX_BUFFER_SIZE];
+        let rx_bytes = read(client_sockfd, &mut buffer)? as usize;
+        let rx_msg = std::str::from_utf8(&buffer[0..rx_bytes]).unwrap_or("");
+        println!("[rx_len={rx_bytes}] <- {rx_msg:?}");
 
-    // 5. read()
-    let mut buffer: RxBuffer = unsafe { mem::zeroed() };
-    let rx_bytes = read(client_sockfd, &mut buffer)?;
-    let s = std::str::from_utf8(&buffer).unwrap_or("");
-    println!("[rx_len={rx_bytes}] <- {s}");
+        // 6
+        let message = "PONG\n\n";
+        let tx_bytes = write(client_sockfd, message.as_bytes())?;
+        println!("[tx_len={tx_bytes}] -> {message:?}");
 
-    // 6. write()
-    let message = "PONG";
-    let tx_bytes = write(client_sockfd, message.as_bytes())?;
-    println!("[tx_len={tx_bytes}] -> {message}");
+        // 7
+        close(client_sockfd)?;
+    }
 
-    // 7. close()
-    close(client_sockfd)?;
-
-    // 8. close()
+    // 8, TODO: cleanup listen socket after receiving signal
     close(listen_sockfd)?;
 
     Ok(())
